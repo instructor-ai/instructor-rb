6ce73e29bbf3f62981eada9a71f130c00f9d5a70
Sergio Bayona <sbayona@martianinteractive.com>
2024-05-28 10:03:14 -0500

    return api error responses

--- lib/instructor/anthropic/response.rb
+++ lib/instructor/anthropic/response.rb
@@ -13,23 +13,30 @@ module Instructor
         @response = response
       end
 
-      # Returns the chat completions from the response.
+      # Parses the function response(s) and returns the parsed arguments.
       #
-      # @return [Array] An array of chat completions.
+      # @return [Array, Hash] The parsed arguments.
+      # @raise [StandardError] if the api response contains an error.
+      def parse
+        raise StandardError, error_message if error?
+
+        if single_response?
+          arguments.first
+        else
+          arguments
+        end
+      end
+
+      private
+
       def content
         @response['content']
       end
 
-      # Returns the tool calls from the chat completions.
-      #
-      # @return [Hash, nil] The tool calls or nil if not found.
       def tool_calls
         content.is_a?(Array) && content.select { |c| c['type'] == 'tool_use' }
       end
 
-      # Checks if there is only a single function response.
-      #
-      # @return [Boolean] True if there is only a single function response, false otherwise.
       def single_response?
         tool_calls&.size == 1
       end
@@ -38,23 +45,12 @@ module Instructor
         tool_calls.map { |tc| tc['input'] }
       end
 
-      # Parses the function response(s) and returns the parsed arguments.
-      #
-      # @return [Array, Hash] The parsed arguments.
-      def parse
-        if single_response?
-          arguments.first
-        else
-          arguments
-        end
+      def error?
+        @response['type'] == 'error'
       end
 
-      # Returns the arguments of the function with the specified name.
-      #
-      # @param function_name [String] The name of the function.
-      # @return [Hash, nil] The arguments of the function or nil if not found.
-      def by_function_name(function_name)
-        function_responses&.find { |res| res['name'] == function_name }&.dig('arguments')
+      def error_message
+        "#{@response.dig('error', 'type')} - #{@response.dig('error', 'message')}"
       end
     end
   end
